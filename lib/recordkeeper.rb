8708f908d482f26d30bc74084b171bd64c2ee7b3
Susan Yi <yi.susans@gmail.com>
2016-08-17 09:34:43 -0400

    create recordkeeper method

--- lib/recordkeeper.rb
+++ lib/recordkeeper.rb
@@ -1,5 +1,14 @@
 require "recordkeeper/version"
 
-module Recordkeeper
-  # Your code goes here...
+class Array
+  def recordkeeper
+    arr = self.uniq
+    hash = Hash[arr.map { |elm| [elm, 0] } ]
+    for i in self
+      hash.each do |key, value|
+        hash[key] = value + 1 if key == i
+      end
+    end
+    hash
+  end
 end
--- lib/recordkeeper/version.rb
+++ lib/recordkeeper/version.rb
@@ -1,3 +1,3 @@
 module Recordkeeper
-  VERSION = "0.1.0"
+  VERSION = "0.0.1"
 end
